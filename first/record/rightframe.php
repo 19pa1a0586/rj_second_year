5cc7bff3fcb24dffa61169433c8d693b14934777
19pa1a0586 <60594951+19pa1a0586@users.noreply.github.com>
2021-09-13 09:11:11 +0530

    Add files via upload

---
+++ first/record/rightframe.php
@@ -0,0 +1,26 @@
+<html>
+
+<body bgcolor="pink">
+    <p>
+    <h2 align="center">
+        <font face="times new roman" color="green">Welcome to Online Book
+            Store </font>
+    </h2>
+    <h2 align="center"> &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;<font face="monotype corsiva" color=blue> This is the online
+            book store developed by students of VIT. </font>
+    </h2>
+    </p>
+    <h1 align="center">
+        <font face="times new roman" color="Red">Latest books
+    </h1>
+    &nbsp;&nbsp;&nbsp;
+    <a href="images/c1.png"><img src="images/c_sharp_6.jpg" height="300" width="200" /></a>
+    &nbsp;&nbsp;&nbsp;
+    <a href="images/c++.png"><img src="images/c_14_quick.jpg" height="300" width="200" /> </a>&nbsp;&nbsp;&nbsp;
+    <a href="images/and.png"> <img src="images/android_studio.jpg" height="300" width="200" /></a>&nbsp;&nbsp;&nbsp;
+    <a href="images/asp.png"> <img src="images/pro_asp4.jpg" height="300" width="200" />
+    </a>&nbsp;&nbsp;&nbsp;
+    <img src="images/mobile_app.jpg" height="300" width="200" />&nbsp;&nbsp;&nbsp;
+</body>
+
+</html>
