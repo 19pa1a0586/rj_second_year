644dca80cf50f3b4ade98abb4141117bc78e6d79
Siva Prasad . K <admin@LAPTOP-HP5L40GL>
2021-09-13 09:31:21 +0530

    5th added

---
+++ fifth program/Login.php
@@ -0,0 +1,19 @@
+<html>
+
+<head>
+</head>
+
+<body>
+    <div>
+        <h1> Login Form </h1>
+        <form action="setcookie.php" method="post">
+            <label> Enter UserName: </label>
+            <input type="text" name="user" /> <br /> <br />
+            <label> Enter Password: </label>
+            <input type="password" name="pwd" /> <br /> <br />
+            <button> LogIn</button>
+        </form>
+    </div>
+</body>
+
+</html>
