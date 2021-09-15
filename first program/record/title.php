1e3a7352028d8bb83d65461cbe1403d5cc56c8da
Siva Prasad . K <admin@LAPTOP-HP5L40GL>
2021-09-15 16:49:14 +0530

    1st modified

--- first program/record/title.php
+++ first program/record/title.php
@@ -1,11 +1,10 @@
 <html>
 
 <head>
-    <script type="text/css">
-</script>
+    <script type="text/css"></script>
 </head>
 
-<body style="font-family:Times New Roman; font-size: 50px;color: red;background-color: lightgreen;">
+<body style="font-family:Times New Roman; font-size: 50px; color: red; background-color: lightgreen;">
     <center><i>ONLINE BOOK STORE </i></center>
     </div>
 </body>
