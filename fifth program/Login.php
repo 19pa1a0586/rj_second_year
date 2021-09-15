fbe3bc2763bcb0de67a5590875be14043efc829b
Siva Prasad . K <admin@LAPTOP-HP5L40GL>
2021-09-15 12:19:05 +0530

    5th modified

--- fifth program/Login.php
+++ fifth program/Login.php
@@ -8,9 +8,9 @@
         <h1> Login Form </h1>
         <form action="setcookie.php" method="post">
             <label> Enter UserName: </label>
-            <input type="text" name="user" /> <br /> <br />
+            <input type="text" name="user"> <br> <br>
             <label> Enter Password: </label>
-            <input type="password" name="pwd" /> <br /> <br />
+            <input type="password" name="pwd"> <br> <br>
             <button> LogIn</button>
         </form>
     </div>
--- fifth program/getcookie.php
+++ fifth program/getcookie.php
@@ -6,39 +6,35 @@
 
 <body>
     <?php 
-$user = $_COOKIE['user']; 
-$pwd= $_COOKIE['pwd']; 
-if($user=="user1") 
-{ 
-if($pwd == "pwd1") 
-echo "<h2>Welcome User1 </h2>"; 
-else 
-echo "<h2>You are not an authenticated user.</h2>"; 
-} 
-elseif($user=="user2") 
-{ 
-if($pwd == "pwd2") 
-echo "<h2>Welcome User2</h2>"; 
-else 
-echo "<h2>You are not an authenticated user.</h2>"; 
-} 
-elseif($user=="user3") 
-{ 
-if($pwd == "pwd3") 
-echo "<h2>Welcome User3</h2>"; 
-else 
-echo "<h2>You are not an authenticated user.</h2>"; 
-} 
-elseif($user=="user4") 
-{ 
-if($pwd == "pwd4") 
-echo "<h2>Welcome User4</h2>"; 
-else 
-echo "<h2>You are not an authenticated user.</h2>"; 
-} 
-else 
-echo "<h2>Invalid Username/Password</h2>"; 
-?>
+        $user = $_COOKIE['user']; 
+        $pwd= $_COOKIE['pwd']; 
+        if($user=="user1"){ 
+            if($pwd == "pwd1") 
+                echo "<h2>Welcome User1 </h2>"; 
+            else 
+                echo "<h2>You are not an authenticated user.</h2>"; 
+        } 
+        elseif($user=="user2"){ 
+            if($pwd == "pwd2") 
+                echo "<h2>Welcome User2</h2>"; 
+            else 
+                echo "<h2>You are not an authenticated user.</h2>"; 
+        } 
+        elseif($user=="user3"){ 
+            if($pwd == "pwd3") 
+                echo "<h2>Welcome User3</h2>"; 
+            else 
+                echo "<h2>You are not an authenticated user.</h2>"; 
+        } 
+        elseif($user=="user4"){ 
+            if($pwd == "pwd4") 
+                echo "<h2>Welcome User4</h2>"; 
+            else 
+                echo "<h2>You are not an authenticated user.</h2>"; 
+        } 
+        else 
+            echo "<h2>Invalid Username/Password</h2>"; 
+    ?>
     <a href="logout.php"> Log Out </a>
 </body>
 
